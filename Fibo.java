ae3b6a232f91c6fdefcd748b9df21c66f97636b8
SANJEETHA .K <sanjeetha.kcse@ksrce.ac.in>
2016-07-22 14:50:00 +0530

    Create Fibo.java

---
+++ Fibo.java
@@ -0,0 +1,13 @@
+public class Fibo{
+       public static void main(String[] args) {
+        int n1=-1;
+       int n2=1;
+       int sum=0;
+       for(int i=0;i<input1;i++){
+    	   sum=n1+n2;
+    	   n1=n2;
+    	   n2=sum;  
+    	   system.out.println(sum);
+       
+    }
+    }
